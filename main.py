b0daed8296862ca9a59d8427adb052d90dcaacf2
Nikolay Andreev <bakasaru@list.ru>
2026-01-16 18:52:16 +0300

    feat: add simple keyboard

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import os
 import telebot
 import dotenv
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 dotenv.load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
+	keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+	keyboard.add(
+		KeyboardButton('Отправить заявку'),
+		KeyboardButton('Посмотреть заявки')
+	)
+	bot.reply_to(message, 'Выберите действие', reply_markup=keyboard)
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(func=lambda message: True)
